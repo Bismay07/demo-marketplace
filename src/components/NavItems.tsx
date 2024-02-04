64980b6a3aba7246568d9625033c8a956fe357c6
Bismay Bibhu Prakash <bismayssp1540@gmail.com>
2024-02-05 00:45:20 +0530

    Day 1- Done

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,49 @@
+"use client"
+import { PRODUCT_CATEGORIES } from "@/config"
+import { useRef, useState } from "react"
+import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
+
+const Navitems = () => {
+
+    const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+    const navRef = useRef<HTMLDivElement | null>(null)
+
+    useOnClickOutside(navRef, () => setActiveIndex(null))
+
+    const isAnyOpen = activeIndex !== null
+
+    return (
+        <>
+            <div className="flex gap-4 h-full" ref={navRef}>
+                {PRODUCT_CATEGORIES.map((category, index) => {
+
+                    const handleOpen = () => {
+                        if (activeIndex === index) {
+                            setActiveIndex(null)
+                        } else {
+                            setActiveIndex(index)
+                        }
+                    }
+
+                    const isOpen = index === activeIndex
+
+
+
+                    return (
+                        <>
+                            <NavItem
+                                category={category}
+                                handleOpen={handleOpen}
+                                isAnyOpen={isAnyOpen}
+                                isOpen={isOpen} />
+                        </>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
+
+export default Navitems
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+
+import Link from "next/link"
+import MaxWidthWrapper from "./MaxWidthWrapper"
+import { Icons } from "./icons"
+import NavItems from "./NavItems"
+
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="bg-white z-50 sticky top-0 inset-x-0 h-16">
+                <header className="relative bg-white">
+                    <MaxWidthWrapper>
+                        <div className="border-b border-gray-200">
+                            <div className="flex h-16 items-center">
+                                {/* TODO: Mobile navbar */}
+
+
+                                <div className="ml-4 flex lg:ml-0">
+                                    <Link href='/'>
+                                        <Icons.logo className="h-10 w-10"/>
+                                    </Link>
+                                </div>
+
+                                <div className="hidden x-50 lg:ml-8 lg:block lg:self-stretch">
+                                    <NavItems />
+                                </div>
+                            </div>
+                        </div>
+                    </MaxWidthWrapper>
+                </header>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
